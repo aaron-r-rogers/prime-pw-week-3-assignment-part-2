320db9a7e81886fbdc4834a5452a8f558364a745
aaron-r-rogers <rogers.aaron.r@gmail.com>
2021-11-10 13:58:17 -0600

    Requirements complete, return for strech goals

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,35 +6,57 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i = 0; i < 4; i++) {
   console.log(i);
 }
 
-// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
+// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+} //changed max (statement 2) to one more than number to end on
+/* This is the auto-fill template atom gives as "for":
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
+It gives me a syntax error when translating our example.  Would like to understand the difference.
+*/
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-
+for (let i = 3; i < 6; i++) {
+  console.log(i);
+} //changed initialization
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-
+for (let i = 2; i < 11; i += 2) {
+  console.log(i);
+} //adds two at each increment
+/*
+Could also do this:
+for (let i = 2; i < 11 ; i++) {
+  if ((i % 2) === 0)
+  console.log(i);
+}
+*/
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
-
-// 2. For of loops  
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
+// 2. For of loops
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+for (each of stars) {
+  console.log(stars);
+}//Runs through 5 times; why?
 
-
-// 3. While loops  
+// 3. While loops
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
